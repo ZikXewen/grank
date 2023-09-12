7e0a40bcce3a813e33e4412b8dc5bad32515b894
ZikXewen <datasup12@gmail.com>
2023-09-12 17:49:51 -0500

    pritel: implement ws upgrade

--- pritel/index.ts
+++ pritel/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import { getUserByUserId } from "@grank/druzi";
+
+Bun.serve({
+  async fetch(req, server) {
+    const cookies = req.headers.get("Cookie");
+    if (!cookies?.startsWith("userId="))
+      return new Response("Unauthorized", { status: 401 });
+    const userId = cookies.split("=")[1];
+    try {
+      const user = await getUserByUserId(userId);
+      if (user.expireAt < Date.now())
+        return new Response("Forbidden", { status: 403 });
+    } catch {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    return server.upgrade(req)
+      ? undefined
+      : new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    async message(ws, msg) { },
+  },
+});
